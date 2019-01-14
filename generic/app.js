986a5667173c66b537158a2da78b8b9f33a5395a
William Bautista <wbautista@seedem.co>
2019-01-14 16:59:19 -0500

    Decoradores

--- generic/app.js
+++ generic/app.js
@@ -5,3 +5,31 @@ function regresar(arg) {
 console.log(regresar('String').toLowerCase());
 console.log(regresar(1997.23423).toFixed(2));
 console.log(new Date().getDate());
+function genericFunction(arg) {
+    return arg;
+}
+var heroe = {
+    nombre: 'Super Man',
+    nombreReal: 'Clark Kent',
+    poder: 'Super fuerza'
+};
+console.log(genericFunction(heroe));
+var heroes = ["Iron Man", "Capitan America", "Hulk"];
+heroes.push("Thor");
+console.log(heroes);
+var villains = ["Lex Lutor", "Flash reverso"];
+villains.push("Lucky");
+console.log(villains);
+var Cuadrado = (function () {
+    function Cuadrado() {
+    }
+    Cuadrado.prototype.area = function () {
+        return +this.base * +this.altura;
+    };
+    ;
+    return Cuadrado;
+}());
+var cuadrado = new Cuadrado();
+cuadrado.base = 20;
+cuadrado.altura = "10";
+console.log(cuadrado.area());
--- generic/app.ts
+++ generic/app.ts
@@ -1,7 +1,7 @@
 // Una función que puede recibir cualquier parametro sin importar su tipo pero de igual manera typescript
 // detecta su tipo de dato con "<T>(arg:T"
 
-function regresar<T>(arg:T) {
+function regresar<T>(arg:T):T {
     return arg;
 }
 
@@ -9,3 +9,50 @@ function regresar<T>(arg:T) {
 console.log( regresar('String').toLowerCase() );
 console.log( regresar(1997.23423).toFixed(2) );
 console.log( new Date().getDate() );
+
+// Function
+function genericFunction<T>( arg:T ):T {
+    return arg;
+}
+
+type Villain = {
+    nombre:string;
+    nombreReal:string;
+}
+
+interface Heroe {
+    nombre:string;
+    poder:string;
+}
+
+let heroe = {
+    nombre: 'Super Man',
+    nombreReal: 'Clark Kent',
+    poder: 'Super fuerza'
+}
+
+console.log( genericFunction<Heroe>( heroe ) );
+
+// Array
+let heroes:Array<string> = ["Iron Man", "Capitan America", "Hulk"];
+heroes.push("Thor");
+console.log(heroes);
+
+let villains:string[] = ["Lex Lutor", "Flash reverso"];
+villains.push("Lucky");
+console.log(villains);
+
+// Clases
+class Cuadrado<T extends number|string> {
+    base:T;
+    altura:T;
+    area():number {
+        return +this.base * +this.altura;
+    };
+}
+
+let cuadrado = new Cuadrado<number|string>();
+
+cuadrado.base = 20;
+cuadrado.altura = "10";
+console.log( cuadrado.area() );
